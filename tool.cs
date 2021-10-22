1258d46272471aea82677e5bd0e6ed5fba5d1ad6
Neon <82705218+NotNeonDEV@users.noreply.github.com>
2021-10-22 15:17:39 +0530

    Add files via upload

--- tool.cs
+++ tool.cs
@@ -5,6 +5,66 @@ using Newtonsoft.Json;
 
 namespace FN_Tool_CSharp
 {
+
+    public class Rootobject
+    {
+        public Channels channels { get; set; }
+        public float cacheIntervalMins { get; set; }
+        public DateTime currentTime { get; set; }
+    }
+
+    public class Channels
+    {
+        public StandaloneStore standalonestore { get; set; }
+
+        [JsonProperty("client-events")]
+        public ClientEvents clientevents { get; set; }
+    }
+
+    public class StandaloneStore
+    {
+        public DateTime cacheExpire { get; set; }
+    }
+
+    public class ClientEvents
+    {
+        public State10[] states { get; set; }
+        public DateTime cacheExpire { get; set; }
+    }
+
+    public class State10
+    {
+        public DateTime validFrom { get; set; }
+        public Activeevent1[] activeEvents { get; set; }
+    }
+
+    public class Sectionstoreends
+    {
+        public DateTime DC8B { get; set; }
+        public DateTime DC9B { get; set; }
+        public DateTime SpookyOffers { get; set; }
+        public DateTime SpookyOffers2 { get; set; }
+        public DateTime SpookyOffers3 { get; set; }
+        public DateTime DC2B { get; set; }
+        public DateTime DC3B { get; set; }
+        public DateTime DC4B { get; set; }
+        public DateTime DC5B { get; set; }
+        public DateTime SpookyOffers4 { get; set; }
+        public DateTime SpookyOffers5 { get; set; }
+        public DateTime DC6B { get; set; }
+        public DateTime DC7B { get; set; }
+        public DateTime Dune { get; set; }
+        public DateTime Featured { get; set; }
+        public DateTime Daily { get; set; }
+    }
+
+    public class Activeevent1
+    {
+        public string eventType { get; set; }
+        public DateTime activeUntil { get; set; }
+        public DateTime activeSince { get; set; }
+    }
+
     class Program
     {
         static void AES()
@@ -53,7 +113,7 @@ namespace FN_Tool_CSharp
             dynamic json = JsonConvert.DeserializeObject(jsonResponse.Content);
 
             if (json != null)
-            {                
+            {
                 foreach (var data in json)
                 {
                     Console.WriteLine($"\nServer Status: {data.status}");
@@ -101,7 +161,11 @@ namespace FN_Tool_CSharp
                 {
                     Console.WriteLine($"\nServer Status: {data.status}\n");
 
-                    if (data.data != null)
+                    if (data.data == null)
+                    {
+                        Console.WriteLine("No Emergency Notice update currently available.");
+                    }
+                    else if (data.data != null)
                     {
                         foreach (var notice in data.data)
                         {
@@ -111,10 +175,6 @@ namespace FN_Tool_CSharp
                             Console.WriteLine($"\nEmergency Notice Title: {notice.title}");
                             Console.WriteLine($"Emergency Notice Message: {notice.body}");
                         }
-                    }
-                    else if (data.data == null)
-                    {
-                        Console.WriteLine("No Emergency Notice update currently available.");
                     }
                 }
             }
@@ -184,48 +244,103 @@ namespace FN_Tool_CSharp
                 }
             }
         }
+        static void EventFlag()
+        {
+            Console.Title = "Event Flag Grabber C#";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            string api = "https://benbot.app/api/v1/calendar";
+            RestClient client = new RestClient(api);
+            IRestRequest jsonRequest = new RestRequest();
+            IRestResponse jsonResponse = client.Execute(jsonRequest);
+            jsonResponse.Content = jsonResponse.Content;
+
+            var Deserializedjson = JsonConvert.DeserializeObject<Rootobject>(jsonResponse.Content);
+
+            if (Deserializedjson != null)
+            {
+                Console.WriteLine("Server Status: 200");
+
+                foreach (var valid in Deserializedjson.channels.clientevents.states)
+                {
+                    Console.WriteLine($"Client Events: {valid.validFrom}\n");
+
+                    if (valid.activeEvents != null)
+                    {
+                        foreach (var flag in valid.activeEvents)
+                        {
+                            var type = flag.eventType;
+                            var activeSince = flag.activeSince;
+                            var activeUntil = flag.activeUntil;
+                            Console.WriteLine($"\nEvent Type: {type} --> Active Since: {activeSince} --> Active Until: {activeUntil}");
+                        }
+                    }
+                }
+
+                Console.WriteLine($"\n\nCache Interval: {Deserializedjson.cacheIntervalMins} Minutes\nCurrent Time: {Deserializedjson.currentTime}");
+            }
+        }
+
+        // MAIN METHOD HERE 
         static void Main(string[] args)
         {
             Console.Title = "FN Tool";
             Console.ForegroundColor = ConsoleColor.Green;
 
             Console.WriteLine("Specify an operation.\n");
-            Console.WriteLine("\tAES");
-            Console.WriteLine("\tCosmetics");
-            Console.WriteLine("\tEmergency Notice");
-            Console.WriteLine("\tFiles");
-            Console.WriteLine("\tShop Tab");
+            Console.WriteLine("\t[1] AES");
+            Console.WriteLine("\t[2] Cosmetics");
+            Console.WriteLine("\t[3] Emergency Notice");
+            Console.WriteLine("\t[4] Files");
+            Console.WriteLine("\t[5] Shop Tab");
+            Console.WriteLine("\t[6] EventFlags");
             string ask = Console.ReadLine();
 
-            if (ask == "AES")
+            if (ask == "1")
             {
                 AES();
                 Console.WriteLine("\n\nProcess finished with exit code 0.");
                 Console.ReadKey();
             }
-            else if (ask == "Cosmetics")
+            else if (ask == "2")
             {
                 Cosmetics();
                 Console.WriteLine("\n\nProcess finished with exit code 0.");
                 Console.ReadKey();
             }
-            else if (ask == "Emergency Notice")
+            else if (ask == "3")
             {
                 Notice();
                 Console.WriteLine("\n\nProcess finished with exit code 0.");
                 Console.ReadKey();
             }
-            else if (ask == "Files")
+            else if (ask == "4")
             {
                 Files();
                 Console.WriteLine("\n\nProcess finished with exit code 0.");
                 Console.ReadKey();
             }
-            else if (ask == "Shop Tab")
+            else if (ask == "5")
             {
                 ShopTab();
                 Console.WriteLine("\n\nProcess finished with exit code 0.");
                 Console.ReadKey();
+            }
+            else if (ask == "6")
+            {
+                EventFlag();
+                Console.WriteLine("\n\nProcess finished with exit code 0.");
+                Console.ReadKey();
+            }
+            else if (ask == "all")
+            {
+                AES();
+                Notice();
+                Cosmetics();
+                ShopTab();
+                Files();
+                EventFlag();
+                Console.WriteLine("\n\n\nProcess finished with exit code 0");
             }
         }
     }
