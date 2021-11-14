d043641282018fbc6d30a801b40298a8d4b62c74
thoo0224 <thomasplatschorre@gmail.com>
2021-11-14 16:52:25 +0100

    some small changes

---
+++ src/Objects/Aes.cs
@@ -0,0 +1,16 @@
+﻿namespace FnTool.Objects;
+
+public class Aes
+{
+    public string Build { get; set; }
+    public string MainKey { get; set; }
+    public DynamicAesKey[] DynamicKeys { get; set; }
+    public DateTime Updated { get; set; }
+}
+
+public class DynamicAesKey
+{
+    public string PakFilename { get; set; }
+    public string PakGuid { get; set; }
+    public string Key { get; set; }
+}
---
+++ src/Objects/CreatorCode.cs
@@ -0,0 +1,15 @@
+﻿namespace FnTool.Objects;
+
+public class CreatorCode
+{
+    public string Code { get; set; }
+    public CreatorCodeAccount Account { get; set; }
+    public string Status { get; set; }
+    public bool Verified { get; set; }
+}
+
+public class CreatorCodeAccount
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
